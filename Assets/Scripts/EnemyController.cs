0adb0e7026b4bf7de78cbe442d376dbf9775b4e6
Ido Yehieli <ido.yehieli@gmail.com>
2020-10-19 14:37:55 +0200

    persue when in range

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,6 +9,10 @@ public class EnemyController : MonoBehaviour {
     public Vector3 maxBound = new Vector3(10, 1.25f, 10);
     Vector3 destination;
 
+    public Transform target;
+    public float detectionRange = 5;
+    public float detectionInterval = .5f;
+
     void Start() {
         cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
@@ -17,17 +21,28 @@ public class EnemyController : MonoBehaviour {
     }
 
     float sinceLastChange;
+    float sinceLastDetection;
+    bool inPursuit=false;
     void Update() {
         sinceLastChange -= Time.deltaTime;
+        sinceLastDetection -= Time.deltaTime;
 
-		if (sinceLastChange <= 0) {
+        if (!inPursuit && sinceLastChange <= 0) {
             sinceLastChange = changeInterval;
 
             destination= new Vector3(Random.Range(minBound.x, maxBound.x), Random.Range(minBound.y, maxBound.y), Random.Range(minBound.z, maxBound.z));
 
-            Debug.Log($"new destination {destination.ToString()}");
-
             agent.SetDestination(destination);
         }
+
+        if (sinceLastDetection <= 0) {
+            sinceLastDetection = detectionInterval;
+            if (Vector3.Distance(target.position, transform.position) <= detectionRange) {
+                inPursuit = true;
+            }
+        }
+
+        if(inPursuit)
+            agent.SetDestination(target.position);
     }
 }
