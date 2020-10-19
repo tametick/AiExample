565c9bf61736c13259965b41258fb474811f4a9e
Ido Yehieli <ido.yehieli@gmail.com>
2020-10-19 12:03:32 +0200

    player controller, added collider to ground for mouse clicks

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+    NavMeshAgent agent;
+
+    void Start() {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                agent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
