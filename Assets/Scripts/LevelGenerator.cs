684386b340731aff1d33b4a40aa3d816a4da3476
Ido Yehieli <ido.yehieli@gmail.com>
2020-10-19 12:30:39 +0200

    rebake level after generation (simple level generator)

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public NavMeshSurface surface;
+    public int width = 20;
+    public int height = 20;
+
+    public GameObject wall;
+    public GameObject player;
+
+    void Start() {
+        GenerateLevel();
+        surface.BuildNavMesh();
+    }
+
+    // Create a grid based level
+    void GenerateLevel() {
+        bool playerSpawned = false;
+        // Loop over the grid
+        for (int x = 0; x <= width; x += 2) {
+            for (int y = 0; y <= height; y += 2) {
+                // Should we place a wall?
+                if (Random.value > .7f) {
+                    // Spawn a wall
+                    Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
+                    Instantiate(wall, pos, Quaternion.identity, transform);
+                } else if (!playerSpawned) // Should we spawn a player?
+                {
+                    // Spawn the player
+                    Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
+                    Instantiate(player, pos, Quaternion.identity);
+                    playerSpawned = true;
+                }
+            }
+        }
+    }
+}
