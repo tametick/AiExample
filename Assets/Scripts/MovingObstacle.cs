906e8272dd950993b54778b5cacbe0149f79ae8a
Ido Yehieli <ido.yehieli@gmail.com>
2020-10-19 12:21:08 +0200

    dyanmic navigation (NavMeshObstacle, Carve, CarveOnStationary)

---
+++ Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour {
+
+    public float speed = .2f;
+    public float strength = 9f;
+
+    private float randomOffset;
+
+    // Use this for initialization
+    void Start() {
+        randomOffset = Random.Range(0f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Sin(Time.time * speed + randomOffset) * strength;
+        transform.position = pos;
+    }
+}
